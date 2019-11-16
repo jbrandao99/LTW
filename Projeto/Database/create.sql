a3922f6a32b0374dbb38ec05cc093b404385c9d6
Muriel Pinho <36748470+MurielPinho@users.noreply.github.com>
2019-11-16 17:34:41 +0000

    Update create.sql

--- Projeto/Database/create.sql
+++ Projeto/Database/create.sql
@@ -3,73 +3,62 @@ DROP TABLE IF EXISTS Owners;
 DROP TABLE IF EXISTS Tourists;
 DROP TABLE IF EXISTS Properties;
 DROP TABLE IF EXISTS Messages;
-DROP TABLE IF EXISTS Comments;
 DROP TABLE IF EXISTS Reservations;
 DROP TABLE IF EXISTS Photos;
 
 CREATE TABLE Users
 (
-    userID INTEGER PRIMARY KEY,
-    userUsername VARCHAR NOT NULL UNIQUE,
-    userEmail VARCHAR NOT NULL UNIQUE,
-    userPassword VARCHAR NOT NULL,
-    userName VARCHAR NOT NULL,
-    userGender CHAR NOT NULL CHECK(userGender = 'M' OR userGender = 'F' OR userGender = 'O'),
-    userBirthDate DATE NOT NULL,
-    userProfilePicture VARCHAR
+    id INTEGER PRIMARY KEY,
+    username VARCHAR NOT NULL UNIQUE,
+    email VARCHAR NOT NULL UNIQUE,
+    password VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    gender CHAR NOT NULL CHECK(userGender = 'M' OR userGender = 'F' OR userGender = 'O'),
+    birthDate DATE NOT NULL,
+    profilePicture VARCHAR
 );
 
 CREATE TABLE Owners
 (
-    ownerID INTEGER PRIMARY KEY REFERENCES Users
+    id INTEGER PRIMARY KEY REFERENCES Users
 );
 
 CREATE TABLE Tourists
 (
-    touristID INTEGER PRIMARY KEY REFERENCES Users
+    id INTEGER PRIMARY KEY REFERENCES Users
 );
 
-CREATE TABLE Rental
+CREATE TABLE Properties
 (
-    rentalID INTEGER PRIMARY KEY,
-    rentalPrice FLOAT NOT NULL,
-    rentalTitle VARCHAR NOT NULL,
-    rentalLocation VARCHAR NOT NULL,
-    rentalDescription VARCHAR NOT NULL,
+    id INTEGER PRIMARY KEY,
+    price FLOAT NOT NULL,
+    title VARCHAR NOT NULL,
+    location VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
     ownerID INTEGER NOT NULL REFERENCES Owners
 );
 
 CREATE TABLE Messages
 (
-    messageID INTEGER PRIMARY KEY,
-    messageText VARCHAR NOT NULL,
+    id INTEGER PRIMARY KEY,
+    text VARCHAR NOT NULL,
 
-);
-
-CREATE TABLE Replies
-(
-    replyID INTEGER PRIMARY KEY,
-    replyText VARCHAR NOT NULL,
-    replyRating INTEGER NOT NULL CHECK (replyRating > 1 AND replyRating <= 5)
-);
-
-CREATE TABLE Comments
-(
-    commentID INTEGER PRIMARY KEY,
-    commentText VARCHAR NOT NULL,
-    commentRating INTEGER NOT NULL CHECK (commentRating > 1 AND commentRating <= 5)
 );
 
 CREATE TABLE Reservations
 (
-    reservationID INTEGER PRIMARY KEY,
-    reservationBeginDate DATE NOT NULL,
-    reservationEndDate DATE NOT NULL,
-    reservationPrice FLOAT NOT NULL
+    id INTEGER PRIMARY KEY,
+    startDate DATE NOT NULL,
+    endDate DATE NOT NULL,
+    price FLOAT NOT NULL,
+    touristComment VARCHAR NOT NULL,
+    touristRating VARCHAR NOT NULL,
+    ownwerComment VARCHAR NOT NULL,
+    ownerRating VARCHAR NOT NULL
 );
 
 CREATE TABLE Photos
 (
-    photoID INTEGER PRIMARY KEY,
-    photoDescription VARCHAR NOT NULL
+    id INTEGER PRIMARY KEY,
+    description VARCHAR NOT NULL
 );
