519df63940f60e79f47f9f43a16fb74491ecda85
João Brandão <up201705573@fe.up.pt>
2019-12-19 12:50:13 +0000

    Contact page working

--- Projeto/actions/action_contact.php
+++ Projeto/actions/action_contact.php
@@ -1,24 +1,16 @@
 <?php
   include_once('../includes/session.php');
 
-    require('../phpmailer/PHPMailerAutoload.php');
-    $mail= new PHPMailer;
-    $mail->Host= 'smtp.gmail.com';
-    $mail->Port= 587;
-    $mail->SMTPAuth=true;
-    $mail->SMTPSecure='tls';
-    $mail->Username= 'fellipe.ranheri@gmail.com';
-    $mail->Password= 'fellipe09';
-    $mail->SetFrom($_POST['email'], $_POST['name']);
-    $mail->AddAddress('up201705573@fe.up.pt');
-    $mail->isHTML(true);
-    $mail->Subject='Rent-a-place: '.$_POST['subject'];
-    $mail->Body=$_POST['message'];
-    $var1=$_POST['subject'];
-    if (!$mail->send()) {
-        $_SESSION['messages'][] = array('type' => 'error', 'content' => 'Failed to sent message');
-        header('Location: pages/login.php');
-    } else {
-        $_SESSION['messages'][] = array('type' => 'success', 'content' => 'Thanks for your contact '.$_POST['name']);
-        header('Location: ../pages/login.php');
-    }
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mail = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mailTo = "up201705573@fe.up.pt";
+    $headers= "Rent-a-Place: " . $mail;
+    $txt = "You have received an e-mail from " . $name . ".\n" . $message;
+
+    mail($mailTo, $subject, $txt, $headers);
+
+    $_SESSION['messages'][] = array('type' => 'success', 'content' => 'Thanks for your contact '.$_POST['name']);
+    die(header('Location: ../pages/contact.php'));
